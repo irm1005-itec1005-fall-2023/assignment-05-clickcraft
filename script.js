5528488aa49c5fe0c2cb0d6372b7c94cafbcd6cc
Laura Dawe <lauradawe@cmail.carleton.ca>
2023-12-05 12:54:28 -0500

    First go at javascript

--- script.js
+++ script.js
@@ -1,82 +1,33 @@
-//
-//  JS File
-//  YOU CAN REMOVE ALL OF THIS CODE AND START FRESH
-//
+const app = document.getElementById("app")
+const rex = document.getElementById("rex");
+const spike = document.getElementById("spike");
 
-//
-// Variables
-//
+function hop() {
+  if (rex.classList != "jump") {
+    rex.classList.add("jump");
 
-// Constants
-const appID = "app";
-const headingText = "Develop. Preview. Ship.";
-const headingTextIcon = "🚀";
-const projectDueDate = "8 December 2023 11:59";
-
-// Variables
-let countdownDate = new Date(projectDueDate);
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
-//
-// Functions
-//
-
-function calculateDaysLeft(countdownDate) {
-  const now = new Date().getTime();
-  const countdown = new Date(countdownDate).getTime();
-
-  console.log(countdown);
-
-  const difference = (countdown - now) / 1000;
-
-
-  // Countdown passed already
-  if (difference < 1) {
-    return null;
+    setTimeout(function () {
+      rex.classList.remove("jump");
+    }, 500);
   }
-
-
-  const days = Math.floor(difference / (60 * 60 * 24));
-
-  return days;
 }
 
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
+let isAlive = setInterval(checkOverlap, 10);
+  
+function checkOverlap () {
+  // get current dino Y position
+  let dinoTop = parseInt(window.getComputedStyle(rex).getPropertyValue("top"));
+
+  // get current cactus X position
+  let cactusLeft = parseInt(
+    window.getComputedStyle(spike).getPropertyValue("left")
+  );
+
+  // detect collision
+  if (cactusLeft < 45 && cactusLeft > 0 && dinoTop >= 140) {
+    // collision
+    alert("Game Over!");
   }
-
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  const daysLeft = calculateDaysLeft(countdownDate);
-  let headingTextCalculated = headingText;
-
-  if (daysLeft > 1) {
-    headingTextCalculated = headingTextCalculated.concat(
-      " In ",
-      daysLeft.toString(),
-      " days "
-    );
-  }else if (daysLeft === 1) {
-    headingTextCalculated = headingTextCalculated.concat(
-      " Tomorrow"
-    );
-  }
-
-  h1.textContent = headingTextCalculated.concat(headingTextIcon);
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
 }
 
-//
-// Inits & Event Listeners
-//
-
-inititialise();
+app.addEventListener("click", hop);
